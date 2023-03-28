fc6e667329f2bde9f0c1674b95dbae994c215cf9
giantcentipede <97794708+giantcentipede@users.noreply.github.com>
2023-03-28 16:44:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup() {
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifycanvas);
+synth=window.speechSynthesis;
+
+}
+
+function clear_canvas() {
+    background("white")
+}
+
+function preload() {
+    classifier=ml5.imageClassifier("DoodleNet");
+
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+line(pmouseX,pmouseY,mouseX,mouseY)
+    }
+}
+
+function classifycanvas() {
+    classifier.classify(canvas,gotresult)
+}
+
+function gotresult(error,results) {
+    if (error) {
+        console.error(error);
+
+    }
+
+    else{
+        console.log(results);
+        document.getElementById("label").innerHTML="label: "+ results[0].label;
+        document.getElementById("confidence").innerHTML="accuracy: "+ Math.round(results[0].confidence *100) + "%";
+     utterthis=new SpeechSynthesisUtterance(results[0].label);
+     synth.speak(utterthis);  
+    }
+}
